2818e1c2b7b07f02e07119427b98864ac2f91da0
sorryu <rjs041221@gmail.com>
2024-11-17 01:52:55 +0900

    Update phonenumber -> number

--- backend/migrations/V1__create_users_table.sql
+++ backend/migrations/V1__create_users_table.sql
@@ -6,15 +6,15 @@
 /*
 History(ex: 20xx-xx-xx | Modifications | name)
 2024-11-11 | users table creation | sorryu
-2024-11-17 | Add phonenumber | sorryu
+2024-11-17 | Add phone number | sorryu
 
 */
 
 CREATE TABLE users {
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL
-    email VARCHAR(100) UNIQUE NOT NULL
-    phonenumber VARCHAR(20) UNIQUE NOT NULL
-    password_hash VARCHAR(255) NOT NULL
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    number VARCHAR(20) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 }
