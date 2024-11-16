d09b88e73670375fbab25890f2665d8d9a517011
sorryu <rjs041221@gmail.com>
2024-11-17 01:56:55 +0900

    Change from foreign key to References

--- backend/migrations/V2__create_clubs_table.sql
+++ backend/migrations/V2__create_clubs_table.sql
@@ -7,15 +7,14 @@
 History(ex: 20xx-xx-xx | Modifications | name)
 2024-11-11 | clubs table creation | sorryu
 2024-11-17 | Add foreign key, creation_userid | sorryu
+2024-11-17 | Change from foreign key to References | sorryu
 
 */
 
 CREATE TABLE clubs {
     id SERIAL PRIMARY KEY,
-    creation_userid INT NOT NULL, -- need to add foreign key connection
+    creation_userid INT REFERENCES users(id), -- need to add foreign key connection
     name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (creation_userid) REFERENCES users(id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 }
