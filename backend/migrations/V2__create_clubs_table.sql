38221a62726169dfd8f67df361e7b9c195662406
sorryu <rjs041221@gmail.com>
2024-11-17 01:53:36 +0900

    Add foreign key, creation_userid

--- backend/migrations/V2__create_clubs_table.sql
+++ backend/migrations/V2__create_clubs_table.sql
@@ -6,12 +6,16 @@
 /*
 History(ex: 20xx-xx-xx | Modifications | name)
 2024-11-11 | clubs table creation | sorryu
+2024-11-17 | Add foreign key, creation_userid | sorryu
 
 */
 
 CREATE TABLE clubs {
     id SERIAL PRIMARY KEY,
+    creation_userid INT NOT NULL, -- need to add foreign key connection
     name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (creation_userid) REFERENCES users(id)
 }
