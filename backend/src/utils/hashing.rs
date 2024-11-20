43c2b60cc4bb01497d673b66c43f2dff1c3caba5
sorryu <rjs041221@gmail.com>
2024-11-20 18:00:07 +0900

    2024-11-19 | Add fake hash function | sorryu

--- backend/src/utils/hashing.rs
+++ backend/src/utils/hashing.rs
@@ -5,6 +5,10 @@
 
 /*
 History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
-
+2024-11-19 | Add fake hash function | sorryu
 
 */
+
+pub fn hash_password(password: &str) -> String {
+    format!("hashed_{}", password) // 실제로는 bcrypt 등의 라이브러리 사용 필요
+}
