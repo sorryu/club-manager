c8cc36410e780f9132ac87f0391a3bc0975008a5
sorryu <rjs041221@gmail.com>
2024-11-25 18:17:58 +0900

    Create structure Claims

---
+++ backend/src/models/jwt_models.rs
@@ -0,0 +1,23 @@
+// Title: jwt_models
+// Created by sorryu
+// Date: 2024-11-22
+// Description: define jwt models
+
+/*
+History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
+2024-11-22 | Modifications | sorryu
+
+*/
+
+use serde::{ Serialize, Deserialize };
+use std::fmt::Debug;
+pub use jsonwebtoken::Header;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub access: i8, // access rate, 0: free plan user, 1: plus plan user, 2: manager, 3: developer
+    pub id: i32,
+    pub email: String,
+    pub exp: usize, // token expiration time
+    pub iat: usize, // token issuance time
+}
--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod user;
 pub mod club;
 pub mod user_role;
+pub mod jwt_models;
