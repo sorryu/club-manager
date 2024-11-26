1dbdaea86b5c33951329c2eca3bdfdabab7cc4de
sorryu <rjs041221@gmail.com>
2024-11-26 17:47:46 +0900

    2024-11-26 | connect global pool from utils::db_pool | sorryu

--- backend/src/controllers/club_controller.rs
+++ backend/src/controllers/club_controller.rs
@@ -8,70 +8,88 @@ History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
 2024-11-17 | Create club Creation and query API handler functions | sorryu
 2024-11-18 | Create User structure, and Add insert logic of user information into database database lookup logic | sorryu
 2024-11-22 | Connect the correct structure from crate:: | sorryu
+2024-11-26 | connect global pool from utils::db_pool | sorryu
 
 */
 
 use actix_web::{get, post, web, HttpResponse, Responder};
 use sqlx::PgPool;
+use log::error;
 
-use crate::models::club::{ ClubData, ClubRequest, ClubResponse };
+use crate::{
+    models::club::{ ClubData, ClubRequest, ClubResponse },
+    utils::db_pool::pool,
+};
 
 // Insert club into database
-async fn insert_club(pool: &PgPool, club_data: &ClubData) -> Result<(), sqlx::Error> {
+async fn insert_club(db_pool: &PgPool, club_data: &ClubData) -> Result<(), sqlx::Error> {
     sqlx::query!("INSERT INTO clubs (name, creation_userid, description) VALUES ($1, $2, $3)",
         club_data.name,
         club_data.creation_userid,
         club_data.description)
-    .execute(pool)
+    .execute(db_pool)
     .await?;
     Ok(())
 }
 
 // Club creation handler
 #[post("/api/clubs")]
-async fn create_club(pool: web::Data<PgPool>, club_request: web::Json<ClubRequest>) -> impl Responder {
-    // need to add request data processing and database storage logic
-    // Get request data
-    let club_data = club_request.into_inner().into();
+async fn create_club(club_request: web::Json<ClubRequest>) -> impl Responder {
+    let db_pool_guard = pool.lock().await;
+    if let Some(db_pool) = &*db_pool_guard {
+        // need to add request data processing and database storage logic
+        // Get request data
+        let club_data = club_request.into_inner().into();
 
-    match insert_club(pool.get_ref(), &club_data).await {
-        Ok(_) => {
-            HttpResponse::Ok()
-                .json(format!("Club {} created!", club_data.name.clone().unwrap_or_else(|| "unknown".to_string())))
-        },
-        Err(err) => {
-            HttpResponse::InternalServerError().body(format!("Error: {}", err))
-        },
+        match insert_club(db_pool, &club_data).await {
+            Ok(_) => {
+                HttpResponse::Ok()
+                    .json(format!("Club {} created!", club_data.name.clone().unwrap_or_else(|| "unknown".to_string())))
+            },
+            Err(err) => {
+                error!("Error: {:?}", err);
+                HttpResponse::InternalServerError().body(format!("Cannot Create Club: {}", err))
+            },
+        }
+    } else {
+        HttpResponse::InternalServerError().body("Database pool not initialized!")
     }
+}
+
+pub async fn get_all_clubs(db_pool: &PgPool) -> Result<Vec<ClubData>, sqlx::Error> {
+    sqlx::query_as::<_, ClubData>("SELECT id, name, creation_userid, description FROM clubs")
+        .fetch_all(db_pool)
+        .await
 }
 
 // All Club lookup handler
 #[get("/api/clubs")]
-async fn get_clubs(pool: web::Data<PgPool>) -> impl Responder {
-    // need to add database lookup logic
-    let result = sqlx::query_as::<_, ClubData>("SELECT id, name, creation_userid, description FROM clubs")
-        .fetch_all(pool.get_ref())
-        .await;
-
-    match result {
-        Ok(clubs_data) => {
-            let clubs_response: Vec<ClubResponse> = {
-                let mut responses = Vec::new();
-                for club_data in clubs_data {
-                    match club_data.to_response(pool.get_ref()).await {
-                        Ok(response) => responses.push(response),
-                        Err(err) => {
-                            log::error!("Failed to convert club data to response: {:?}", err);
-                            return HttpResponse::InternalServerError().body("Failed to fetch clubs.");
+async fn get_clubs() -> impl Responder {
+    let db_pool_guard = pool.lock().await;
+    if let Some(db_pool) = &*db_pool_guard {
+        match get_all_clubs(db_pool).await {
+            Ok(clubs_data) => {
+                let clubs_response: Vec<ClubResponse> = {
+                    let mut responses = Vec::new();
+                    for club_data in clubs_data {
+                        match club_data.to_response(db_pool).await {
+                            Ok(response) => responses.push(response),
+                            Err(err) => {
+                                log::error!("Failed to convert club data to response: {:?}", err);
+                                return HttpResponse::InternalServerError().body("Failed to fetch clubs.");
+                            }
                         }
                     }
-                }
-                responses
-            };
-            HttpResponse::Ok().json(clubs_response)
-        },
-        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err))
+                    responses
+                };
+                HttpResponse::Ok().json(clubs_response)
+            },
+            Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err))
+        }
+    } else {
+        HttpResponse::InternalServerError().body("Database pool is not initialized.")
     }
+    
 }
 
 // Set the in-module handler
--- backend/src/controllers/user_controller.rs
+++ backend/src/controllers/user_controller.rs
@@ -11,6 +11,7 @@ History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
 2024-11-18 | Create User structure and Add database lookup logic | sorryu
 2024-11-22 | Connect the correct structure | sorryu
 2024-11-25 | use try_into and return Error | sorryu
+2024-11-26 | connect global pool from utils::db_pool | sorryu
 
 */
 
@@ -20,60 +21,76 @@ use actix_web::{get, post, web, HttpResponse, Responder};
 // Responder: Return HTTP Response from Handler Function
 
 use sqlx::PgPool;
+use log::error;
 
-use crate::models::user::{ UserData, UserRequest, UserResponse };
+use crate::{
+    models::user::{ UserData, UserRequest, UserResponse },
+    utils::db_pool::pool,
+};
 
 // Insert user into database
-async fn insert_user(pool: &PgPool, user_data: &UserData) -> Result<(), sqlx::Error> {
-
+async fn insert_user(db_pool: &PgPool, user_data: &UserData) -> Result<(), sqlx::Error> {
     sqlx::query!("INSERT INTO users (username, email, number, password_hash) VALUES ($1, $2, $3, $4)",
         user_data.username,
         user_data.email,
         user_data.number,
         user_data.hashed_password)
-    .execute(pool)
+    .execute(db_pool)
     .await?;
     Ok(())
 }
 
 // User registration handler
 #[post("/api/users")] // Attribute macro, POST HTTP request method path
-async fn create_user(pool: web::Data<PgPool>, user_request: web::Json<UserRequest>) -> impl Responder { // set Responder to Return Value
+async fn create_user(user_request: web::Json<UserRequest>) -> impl Responder { // set Responder to Return Value
     // Get request data
-    let user_data = match user_request.into_inner().try_into() {
-        Ok(data) => data,
-        Err(err) => {
-            return HttpResponse::BadRequest().body(format!("Invalid input: {}", err));
-        }
-    }; // Extract the inner data from web::Json
+    let db_pool_guard = pool.lock().await;
+    if let Some(db_pool) = &*db_pool_guard {
+        let user_data = match user_request.into_inner().try_into() {
+            Ok(data) => data,
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Invalid input: {}", err));
+            }
+        }; // Extract the inner data from web::Json
 
-    match insert_user(pool.get_ref(), &user_data).await {
-        Ok(_) => {
-            HttpResponse::Ok()
-                .json(format!("User {} created!", user_data.username.clone().unwrap_or_else(|| "unknown".to_string())))
-        },
-        Err(err) => {
-            HttpResponse::InternalServerError().body(format!("Error: {}", err))
-        },
+        match insert_user(db_pool, &user_data).await {
+            Ok(_) => {
+                HttpResponse::Ok()
+                    .json(format!("User {} created!", user_data.username.clone().unwrap_or_else(|| "unknown".to_string())))
+            },
+            Err(err) => {
+                error!("Error: {:?}", err);
+                HttpResponse::InternalServerError().body(format!("Cannot Create User: {}", err))
+            },
+        }
+    } else {
+        HttpResponse::InternalServerError().body("Database pool not initialized!")
     }
+}
+
+pub async fn get_all_users(db_pool: &PgPool) -> Result<Vec<UserData>, sqlx::Error> {
+    sqlx::query_as::<_, UserData>("SELECT id, username, number, hashed_password FROM users")
+        .fetch_all(db_pool)
+        .await
 }
 
 // All User lookup handler
 #[get("/api/users")] // GET HTTP request method path
-async fn get_users(pool: web::Data<PgPool>) -> impl Responder {
-    // need to add database lookup logic
-    let result = sqlx::query_as::<_, UserData>("SELECT id, username, number, hashed_password FROM users")
-        .fetch_all(pool.get_ref())
-        .await;
-    
-    match result {
-        Ok(users_data) => {
-            let users_response: Vec<UserResponse> = users_data.into_iter().map(UserData::into).collect();
-            HttpResponse::Ok().json(users_response)
-        },
-        Err(err) => {
-            HttpResponse::InternalServerError().body(format!("Error: {}", err))
-        },
+async fn get_users() -> impl Responder {
+    let db_pool_guard = pool.lock().await;
+    if let Some(db_pool) = &*db_pool_guard {
+        match get_all_users(db_pool).await {
+            Ok(users_data) => {
+                let users_responses: Vec<UserResponse> = users_data.into_iter().map(UserData::into).collect();
+                HttpResponse::Ok().json(users_responses)
+            },
+            Err(err) => {
+                error!("Error: {:?}", err);
+                HttpResponse::InternalServerError().body(format!("Cannot get users from database: {}", err))
+            }
+        }
+    } else {
+        HttpResponse::InternalServerError().body("Database pool is not initialized.")
     }
 }
 
