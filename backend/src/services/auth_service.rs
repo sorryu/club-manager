1de2cc4fa1ad095cf9f59994950a6b468f3f7c05
sorryu <rjs041221@gmail.com>
2024-11-30 21:35:45 +0900

    auth_service Move the insert_user function, get_all_users function, which was on the controller to the service location
    club_service
     Move the insert_club function, get_all_clubs function, which was on the controller to the service location

--- backend/src/services/auth_service.rs
+++ backend/src/services/auth_service.rs
@@ -5,6 +5,27 @@
 
 /*
 History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
-
+2024-11-29 | Move the insert_user function, get_all_users function, which was on the controller to the service location | sorryu
 
 */
+
+use sqlx::{ PgPool, Error };
+use crate::models::user::UserData;
+
+// Insert user into database
+pub async fn insert_user(db_pool: &PgPool, user_data: &UserData) -> Result<(), Error> {
+    sqlx::query!("INSERT INTO users (username, email, number, password_hash) VALUES ($1, $2, $3, $4)",
+        user_data.username,
+        user_data.email,
+        user_data.number,
+        user_data.hashed_password)
+    .execute(db_pool)
+    .await?;
+    Ok(())
+}
+
+pub async fn get_all_users(db_pool: &PgPool) -> Result<Vec<UserData>, Error> {
+    sqlx::query_as::<_, UserData>("SELECT id, username, number, hashed_password FROM users")
+        .fetch_all(db_pool)
+        .await
+}
--- backend/src/services/club_service.rs
+++ backend/src/services/club_service.rs
@@ -5,6 +5,29 @@
 
 /*
 History(ex: 20xx-xx-xx | Modifications(what, how, why) | name)
-
+2024-11-29 | Move the insert_club function, get_all_clubs function, which was on the controller to the service location | sorryu
 
 */
+
+use crate::models::club::ClubData;
+
+use sqlx::{ PgPool, error::Error as SqlError };
+
+// Insert club into database
+pub async fn insert_club(db_pool: &PgPool, club_data: &ClubData) -> Result<(), SqlError> {
+    // Hard coding makes maintenance difficult. Let's consider ORM.
+    sqlx::query!("INSERT INTO clubs (name, creation_userid, description) VALUES ($1, $2, $3)",
+        club_data.name,
+        club_data.creation_userid,
+        club_data.description)
+    .execute(db_pool)
+    .await?;
+    Ok(())
+}
+
+pub async fn get_all_clubs(db_pool: &PgPool) -> Result<Vec<ClubData>, SqlError> {
+    // Let's receive additional parameters to enable filtering and paging.
+    sqlx::query_as::<_, ClubData>("SELECT id, name, creation_userid, description FROM clubs")
+        .fetch_all(db_pool)
+        .await
+}
